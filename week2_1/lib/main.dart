d525c264bcc464230996e5249984a989fa1cf2e5
TawanBBT02 <t0985595716@gmail.com>
2025-12-05 15:59:45 +0700

    test

--- week2_1/lib/main.dart
+++ week2_1/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "MyApp",
+      title: "My App",
       home: MyHomePage(),
       theme: ThemeData(primarySwatch: Colors.deepOrange),
     );
